c167c836b0ac3f11d4d693864d1332a9ab11dbb5
Kmann180 <7319kkennedy@gmail.com>
2015-01-14 17:59:12 -0800

    end of day
    01/14/2015

--- AIE_RTS/Assets/WaifuFrenzy/SelectionRD/Scripts/Managers/BuildingManager.cs
+++ AIE_RTS/Assets/WaifuFrenzy/SelectionRD/Scripts/Managers/BuildingManager.cs
@@ -10,13 +10,20 @@ public class BuildingManager : MonoBehaviour {
     // Building to spawn
     public GameObject BuildingMCV;
     public GameObject BuildingMCVPrimary;
+    public GameObject BuildingResource;
     [SerializeField]
     public bool BuildingMCVPrimBool = false;
     public List<GameObject> BuildingMCVList;
+    public List<GameObject> BuildingResourceList;
+    public float Gold = 3000;
+    public float Income = 0;
+    public string GoldString;
 
     // Clone and bools to manage placing
     public GameObject Clone;
     public bool CloneSpawned = false;
+    public bool CloneSpawnedR = false;
+    public bool BuildingGhost = false;
     
     // Use this for initialization
 	void Start () 
@@ -29,46 +36,79 @@ public class BuildingManager : MonoBehaviour {
     {
         FollowCursor();
         PlaceBuilding();
+        GetGold();
 	}
 
     void OnGUI()
     {
-        // Button to create MCV building clone
-        if (GUI.Button(new Rect(Screen.width - 200, Screen.height / 2, 100, 50), "Spawn Building"))
+        if (BuildingGhost == false)
         {
-            Clone = Instantiate(BuildingMCV) as GameObject;
-            CloneSpawned = true;
-            if(!BuildingMCVPrimBool)
+            // Button to create MCV building clone
+            if (GUI.Button(new Rect(Screen.width - 200, Screen.height / 2, 100, 50), "Spawn Building"))
             {
-                BuildingMCVPrimary = Clone;
-                BuildingMCVPrimBool = true;
+                if (Gold >= 1000)
+                {
+                    Clone = Instantiate(BuildingMCV) as GameObject;
+                    CloneSpawned = true;
+                    BuildingGhost = true;
+                    if (!BuildingMCVPrimBool)
+                    {
+                        BuildingMCVPrimary = Clone;
+                        BuildingMCVPrimBool = true;
+                    }
+                }
             }
-        }
 
-        // Button to spawn unit on primary building
-        if (BuildingMCVPrimBool)
-        {
-            if (GUI.Button(new Rect(Screen.width - 100, Screen.height / 2, 100, 50), "Spawn Units"))
+            // Button to spawn unit on primary building
+            if (BuildingMCVPrimBool)
             {
-                if (BuildingMCVPrimBool)
-                    BuildingMCVPrimary.gameObject.GetComponent<SimpleBuilding>().SpawnUnit();
+                if (GUI.Button(new Rect(Screen.width - 100, Screen.height / 2, 100, 50), "Spawn Units"))
+                {
+                    if (Gold >= 100)
+                    {
+                        if (BuildingMCVPrimBool)
+                            BuildingMCVPrimary.gameObject.GetComponent<SimpleBuilding>().SpawnUnit();
+                        Gold -= 100;
+                    }
+                }
             }
-        }
 
-        // Button to spawn resource building
-        if (BuildingMCVPrimBool)
-        {
-            if (GUI.Button(new Rect(Screen.width - 200, Screen.height / 2 + 50, 100, 50), "Resource"))
+            // Button to spawn resource building
+            if (BuildingMCVPrimBool)
             {
-                Debug.Log("Spawn resource building here");
+                if (GUI.Button(new Rect(Screen.width - 200, Screen.height / 2 + 50, 100, 50), "Resource"))
+                {
+                    if (Gold >= 500)
+                    {
+                        Clone = Instantiate(BuildingResource) as GameObject;
+                        CloneSpawnedR = true;
+                        BuildingGhost = true;
+                        Debug.Log("Spawn resource building here");
+                    }
+                }
             }
         }
+       GoldString = "Gold: " + (int)Gold;
+       GUI.TextField(new Rect(Screen.width - 200, Screen.height / 2 - 20, 100, 20), GoldString);
     }
 
     void FollowCursor()
     {
         // Raycast to keep building clone on cursor
         if (CloneSpawned)
+        {
+            // Raycast
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Ground")
+                {
+                    Clone.transform.position = new Vector3(hit.point.x, 5.0f, hit.point.z);
+                }
+            }
+        }
+        if (CloneSpawnedR)
         {
             // Raycast
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
@@ -88,13 +128,29 @@ public class BuildingManager : MonoBehaviour {
         // When left clicked, building is placed at location
         if (Input.GetMouseButtonDown(0))
         {
+            BuildingGhost = false;
             if (CloneSpawned)
             {
                 CloneSpawned = false;
                 Clone.transform.position = new Vector3(Clone.transform.position.x, 1.0f, Clone.transform.position.z);
                 BuildingMCVList.Add(Clone);
                 BuildingMCV.gameObject.GetComponent<SimpleBuilding>().IsPlaced = true;
+                Gold -= 1000;
+            }
+            if (CloneSpawnedR)
+            {
+                CloneSpawnedR = false;
+                Clone.transform.position = new Vector3(Clone.transform.position.x, 1.0f, Clone.transform.position.z);
+                BuildingResourceList.Add(Clone);
+                BuildingResource.gameObject.GetComponent<SimpleBuilding>().IsPlaced = true;
+                Income += 1;
+                Gold -= 500;
             }
         }
+    }
+
+    void GetGold()
+    {
+        Gold += Time.deltaTime  * Income;
     }
 }
